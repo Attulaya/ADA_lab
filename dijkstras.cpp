ef5f56bda0b4da68b0e213635293c0e23aae0ee9
Attulaya <97424461+Attulaya@users.noreply.github.com>
2024-01-22 01:49:49 +0530

    Update dijkstras.cpp

--- dijkstras.cpp
+++ dijkstras.cpp
@@ -1,107 +1,124 @@
-// C++ program for Dijkstra's single source shortest path
-// algorithm. The program is for adjacency matrix
-// representation of the graph
-#include <iostream>
+// C++ Program to find Dijkstra's shortest path using
+// priority_queue in STL
+#include <bits/stdc++.h>
 using namespace std;
-#include <limits.h>
+#define INF 0x3f3f3f3f
 
-// Number of vertices in the graph
-#define V 9
+// iPair ==> Integer Pair
+typedef pair<int, int> iPair;
 
-// A utility function to find the vertex with minimum
-// distance value, from the set of vertices not yet included
-// in shortest path tree
-int minDistance(int dist[], bool sptSet[])
+// This class represents a directed graph using
+// adjacency list representation
+class Graph {
+	int V; // No. of vertices
+
+	// In a weighted graph, we need to store vertex
+	// and weight pair for every edge
+	list<pair<int, int> >* adj;
+
+public:
+	Graph(int V); // Constructor
+
+	// function to add an edge to graph
+	void addEdge(int u, int v, int w);
+
+	// prints shortest path from s
+	void shortestPath(int s);
+};
+
+// Allocates memory for adjacency list
+Graph::Graph(int V)
 {
-
-	// Initialize min value
-	int min = INT_MAX, min_index;
-
-	for (int v = 0; v < V; v++)
-		if (sptSet[v] == false && dist[v] <= min)
-			min = dist[v], min_index = v;
-
-	return min_index;
+	this->V = V;
+	adj = new list<iPair>[V];
 }
 
-// A utility function to print the constructed distance
-// array
-void printSolution(int dist[])
+void Graph::addEdge(int u, int v, int w)
 {
-	cout << "Vertex \t Distance from Source" << endl;
-	for (int i = 0; i < V; i++)
-		cout << i << " \t\t\t\t" << dist[i] << endl;
+	adj[u].push_back(make_pair(v, w));
+	adj[v].push_back(make_pair(u, w));
 }
 
-// Function that implements Dijkstra's single source
-// shortest path algorithm for a graph represented using
-// adjacency matrix representation
-void dijkstra(int graph[V][V], int src)
+// Prints shortest paths from src to all other vertices
+void Graph::shortestPath(int src)
 {
-	int dist[V]; // The output array. dist[i] will hold the
-				// shortest
-	// distance from src to i
+	// Create a priority queue to store vertices that
+	// are being preprocessed. This is weird syntax in C++.
+	// Refer below link for details of this syntax
+	// https://www.geeksforgeeks.org/implement-min-heap-using-stl/
+	priority_queue<iPair, vector<iPair>, greater<iPair> >
+		pq;
 
-	bool sptSet[V]; // sptSet[i] will be true if vertex i is
-					// included in shortest
-	// path tree or shortest distance from src to i is
-	// finalized
+	// Create a vector for distances and initialize all
+	// distances as infinite (INF)
+	vector<int> dist(V, INF);
 
-	// Initialize all distances as INFINITE and stpSet[] as
-	// false
-	for (int i = 0; i < V; i++)
-		dist[i] = INT_MAX, sptSet[i] = false;
-
-	// Distance of source vertex from itself is always 0
+	// Insert source itself in priority queue and initialize
+	// its distance as 0.
+	pq.push(make_pair(0, src));
 	dist[src] = 0;
 
-	// Find shortest path for all vertices
-	for (int count = 0; count < V - 1; count++) {
-		// Pick the minimum distance vertex from the set of
-		// vertices not yet processed. u is always equal to
-		// src in the first iteration.
-		int u = minDistance(dist, sptSet);
+	/* Looping till priority queue becomes empty (or all
+	distances are not finalized) */
+	while (!pq.empty()) {
+		// The first vertex in pair is the minimum distance
+		// vertex, extract it from priority queue.
+		// vertex label is stored in second of pair (it
+		// has to be done this way to keep the vertices
+		// sorted distance (distance must be first item
+		// in pair)
+		int u = pq.top().second;
+		pq.pop();
 
-		// Mark the picked vertex as processed
-		sptSet[u] = true;
+		// 'i' is used to get all adjacent vertices of a
+		// vertex
+		list<pair<int, int> >::iterator i;
+		for (i = adj[u].begin(); i != adj[u].end(); ++i) {
+			// Get vertex label and weight of current
+			// adjacent of u.
+			int v = (*i).first;
+			int weight = (*i).second;
 
-		// Update dist value of the adjacent vertices of the
-		// picked vertex.
-		for (int v = 0; v < V; v++)
-
-			// Update dist[v] only if is not in sptSet,
-			// there is an edge from u to v, and total
-			// weight of path from src to v through u is
-			// smaller than current value of dist[v]
-			if (!sptSet[v] && graph[u][v]
-				&& dist[u] != INT_MAX
-				&& dist[u] + graph[u][v] < dist[v])
-				dist[v] = dist[u] + graph[u][v];
+			// If there is shorted path to v through u.
+			if (dist[v] > dist[u] + weight) {
+				// Updating distance of v
+				dist[v] = dist[u] + weight;
+				pq.push(make_pair(dist[v], v));
+			}
+		}
 	}
 
-	// print the constructed distance array
-	printSolution(dist);
+	// Print shortest distances stored in dist[]
+	printf("Vertex Distance from Source\n");
+	for (int i = 0; i < V; ++i)
+		printf("%d \t\t %d\n", i, dist[i]);
 }
 
-// driver's code
+// Driver's code
 int main()
 {
+	// create the graph given in above figure
+	int V = 9;
+	Graph g(V);
 
-	/* Let us create the example graph discussed above */
-	int graph[V][V] = { { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
-						{ 4, 0, 8, 0, 0, 0, 0, 11, 0 },
-						{ 0, 8, 0, 7, 0, 4, 0, 0, 2 },
-						{ 0, 0, 7, 0, 9, 14, 0, 0, 0 },
-						{ 0, 0, 0, 9, 0, 10, 0, 0, 0 },
-						{ 0, 0, 4, 14, 10, 0, 2, 0, 0 },
-						{ 0, 0, 0, 0, 0, 2, 0, 1, 6 },
-						{ 8, 11, 0, 0, 0, 0, 1, 0, 7 },
-						{ 0, 0, 2, 0, 0, 0, 6, 7, 0 } };
+	// making above shown graph
+	g.addEdge(0, 1, 4);
+	g.addEdge(0, 7, 8);
+	g.addEdge(1, 2, 8);
+	g.addEdge(1, 7, 11);
+	g.addEdge(2, 3, 7);
+	g.addEdge(2, 8, 2);
+	g.addEdge(2, 5, 4);
+	g.addEdge(3, 4, 9);
+	g.addEdge(3, 5, 14);
+	g.addEdge(4, 5, 10);
+	g.addEdge(5, 6, 2);
+	g.addEdge(6, 7, 1);
+	g.addEdge(6, 8, 6);
+	g.addEdge(7, 8, 7);
 
 	// Function call
-	dijkstra(graph, 0);
+	g.shortestPath(0);
 
 	return 0;
 }
-
-// This code is contributed by shivanisinghss2110
