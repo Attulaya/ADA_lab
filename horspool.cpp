283ac6f065bd75ac3706c2d2de6631602c18f7b0
Attulaya <97424461+Attulaya@users.noreply.github.com>
2024-01-23 00:28:44 +0530

    Update horspool.cpp

--- horspool.cpp
+++ horspool.cpp
@@ -1,89 +1,55 @@
-/* C++ Program for Bad Character Heuristic of Boyer 
-Moore String Matching Algorithm */
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cstring>
+#include <string>
+
 using namespace std;
-# define NO_OF_CHARS 256 
 
-// The preprocessing function for Boyer Moore's 
-// bad character heuristic 
-void badCharHeuristic( string str, int size, 
-						int badchar[NO_OF_CHARS]) 
-{ 
-	int i; 
+char str[100], ptn[20];
+int res, m, n, len, len1, i, j, k, table[1000];
 
-	// Initialize all occurrences as -1 
-	for (i = 0; i < NO_OF_CHARS; i++) 
-		badchar[i] = -1; 
+void shift(char p[])
+{
+    len = strlen(p);
+    for (i = 0; i < 1000; i++)
+        table[i] = len;
+    for (j = 0; j <= len - 2; j++)
+        table[p[j]] = len - 1 - j;
+}
 
-	// Fill the actual value of last occurrence 
-	// of a character 
-	for (i = 0; i < size; i++) 
-		badchar[(int) str[i]] = i; 
-} 
+int horspool(char p[], char t[])
+{
+    shift(p);
+    m = strlen(p);
+    n = strlen(t);
+    i = m - 1;
+    while (i <= n - 1)
+    {
+        k = 0;
+        while (k <= m - 1 && (p[m - 1 - k] == t[i - k]))
+            k++;
+        if (k == m)
+            return i - m + 1;
+        else
+            i = i + table[t[i]];
+    }
+    return -1;
+}
 
-/* A pattern searching function that uses Bad 
-Character Heuristic of Boyer Moore Algorithm */
-void search( string txt, string pat) 
-{ 
-	int m = pat.size(); 
-	int n = txt.size(); 
+int main()
+{
+    cout << "Enter the text: \n";
+    cin.getline(str, sizeof(str));
 
-	int badchar[NO_OF_CHARS]; 
+    cout << "Enter the pattern to be found: \n";
+    cin.getline(ptn, sizeof(ptn));
 
-	/* Fill the bad character array by calling 
-	the preprocessing function badCharHeuristic() 
-	for given pattern */
-	badCharHeuristic(pat, m, badchar); 
+    res = horspool(ptn, str);
 
-	int s = 0; // s is shift of the pattern with 
-				// respect to text 
-	while(s <= (n - m)) 
-	{ 
-		int j = m - 1; 
+    if (res == -1)
+        cout << "\nPattern not found\n";
+    else
+        cout << "Pattern found at " << res + 1 << " position \n";
 
-		/* Keep reducing index j of pattern while 
-		characters of pattern and text are 
-		matching at this shift s */
-		while(j >= 0 && pat[j] == txt[s + j]) 
-			j--; 
-
-		/* If the pattern is present at current 
-		shift, then index j will become -1 after 
-		the above loop */
-		if (j < 0) 
-		{ 
-			cout << "pattern occurs at shift = " << s << endl; 
-
-			/* Shift the pattern so that the next 
-			character in text aligns with the last 
-			occurrence of it in pattern. 
-			The condition s+m < n is necessary for 
-			the case when pattern occurs at the end 
-			of text */
-			s += (s + m < n)? m-badchar[txt[s + m]] : 1; 
-
-		} 
-
-		else
-			/* Shift the pattern so that the bad character 
-			in text aligns with the last occurrence of 
-			it in pattern. The max function is used to 
-			make sure that we get a positive shift. 
-			We may get a negative shift if the last 
-			occurrence of bad character in pattern 
-			is on the right side of the current 
-			character. */
-			s += max(1, j - badchar[txt[s + j]]); 
-	} 
-} 
-
-/* Driver code */
-int main() 
-{ 
-	string txt= "ABAAABCD"; 
-	string pat = "ABC"; 
-	search(txt, pat); 
-	return 0; 
-} 
-
-// This code is contributed by rathbhupendra
+    return 0;
+}
+HORSEPOOL
