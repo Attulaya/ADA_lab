5263ae25ff2f54722f20992c8649e63ac67d6da7
Attulaya <97424461+Attulaya@users.noreply.github.com>
2024-01-22 22:29:24 +0530

    Update prims.cpp

--- prims.cpp
+++ prims.cpp
@@ -1,61 +1,128 @@
-#include<bits/stdc++.h>
+// C++ Program to find Dijkstra's shortest path using
+// priority_queue in STL
+#include <bits/stdc++.h>
 using namespace std;
+#define INF 0x3f3f3f3f
 
-int main(){
-	int N=5,m=6;
-	vector<pair<int,int> > adj[N]; 
-	adj[0].push_back({1,2});
-	adj[0].push_back({3,6});
-	adj[1].push_back({0,2});
-	adj[1].push_back({2,3});
-	adj[1].push_back({3,8});
-	adj[1].push_back({4,5});
-	adj[2].push_back({1,3});
-	adj[2].push_back({4,7});
-	adj[3].push_back({0,6});
-	adj[3].push_back({1,8});
-	adj[4].push_back({1,5});
-	adj[4].push_back({2,7});
+// iPair ==> Integer Pair
+typedef pair<int, int> iPair;
 
+// This class represents a directed graph using
+// adjacency list representation
+class Graph {
+	int V; // No. of vertices
 
+	// In a weighted graph, we need to store vertex
+	// and weight pair for every edge
+	list<pair<int, int> >* adj;
 
-	
-	
-    int parent[N]; 
-      
-    int key[N]; 
-      
-    bool mstSet[N]; 
-  
-    for (int i = 0; i < N; i++) 
-        key[i] = INT_MAX, mstSet[i] = false; 
-    
-    priority_queue< pair<int,int>, vector <pair<int,int>> , greater<pair<int,int>> > pq;
+public:
+	Graph(int V); // Constructor
 
-    key[0] = 0; 
-    parent[0] = -1; 
-    pq.push({0, 0});
+	// function to add an edge to graph
+	void addEdge(int u, int v, int w);
 
-    while(!pq.empty())
-    { 
-        int u = pq.top().second; 
-        pq.pop(); 
-        
-        mstSet[u] = true; 
-        
-        for (auto it : adj[u]) {
-            int v = it.first;
-            int weight = it.second;
-            if (mstSet[v] == false && weight < key[v]) {
-                parent[v] = u;
-	    key[v] = weight; 
-                pq.push({key[v], v});    
-            }
-        }
-            
-    } 
-    
-    for (int i = 1; i < N; i++) 
-        cout << parent[i] << " - " << i <<" \n"; 
+	// prints shortest path from s
+	void shortestPath(int s);
+};
+
+// Allocates memory for adjacency list
+Graph::Graph(int V)
+{
+	this->V = V;
+	adj = new list<iPair>[V];
+}
+
+void Graph::addEdge(int u, int v, int w)
+{
+	adj[u].push_back(make_pair(v, w));
+	adj[v].push_back(make_pair(u, w));
+}
+
+// Prints shortest paths from src to all other vertices
+void Graph::shortestPath(int src)
+{
+	// Create a priority queue to store vertices that
+	// are being preprocessed. This is weird syntax in C++.
+	// Refer below link for details of this syntax
+	// https://www.geeksforgeeks.org/implement-min-heap-using-stl/
+	priority_queue<iPair, vector<iPair>, greater<iPair> >pq;
+
+	// Create a vector for distances and initialize all
+	// distances as infinite (INF)
+	vector<int> dist(V, INF);
+    vector<int> parent(V);
+    vector<bool> mst(V,false); 
+
+	// Insert source itself in priority queue and initialize
+	// its distance as 0.
+	pq.push(make_pair(0, src));
+	dist[src] = 0;
+    parent[src]=-1;
+
+	/* Looping till priority queue becomes empty (or all
+	distances are not finalized) */
+	while (!pq.empty()) {
+		// The first vertex in pair is the minimum distance
+		// vertex, extract it from priority queue.
+		// vertex label is stored in second of pair (it
+		// has to be done this way to keep the vertices
+		// sorted distance (distance must be first item
+		// in pair)
+		int u = pq.top().second;
+		pq.pop();
+        mst[u]=true;
+
+		// 'i' is used to get all adjacent vertices of a
+		// vertex
+		list<pair<int, int> >::iterator i;
+		for (i = adj[u].begin(); i != adj[u].end(); ++i) {
+			// Get vertex label and weight of current
+			// adjacent of u.
+			int v = (*i).first;
+			int weight = (*i).second;
+
+			// If there is shorted path to v through u.
+			if (dist[v] > weight && mst[v]==false) {
+				// Updating distance of v
+                parent[v]=u;
+				dist[v] = dist[u] + weight;
+				pq.push(make_pair(dist[v], v));
+			}
+		}
+	}
+
+	// Print shortest distances stored in dist[]
+	printf("Vertex Distance from Source\n");
+	for (int i = 0; i < V; ++i)
+		printf("%d \t\t %d\n", parent[i], i);
+}
+
+// Driver's code
+int main()
+{
+	// create the graph given in above figure
+	int V = 9;
+	Graph g(V);
+
+	// making above shown graph
+	g.addEdge(0, 1, 4);
+	g.addEdge(0, 7, 8);
+	g.addEdge(1, 2, 8);
+	g.addEdge(1, 7, 11);
+	g.addEdge(2, 3, 7);
+	g.addEdge(2, 8, 2);
+	g.addEdge(2, 5, 4);
+	g.addEdge(3, 4, 9);
+	g.addEdge(3, 5, 14);
+	g.addEdge(4, 5, 10);
+	g.addEdge(5, 6, 2);
+	g.addEdge(6, 7, 1);
+	g.addEdge(6, 8, 6);
+	g.addEdge(7, 8, 7);
+
+	// Function call
+	g.shortestPath(0);
+
 	return 0;
 }
