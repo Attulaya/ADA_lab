8ec0ae3712804527b1b6c9c7d1650c19447d932c
Attulaya <97424461+Attulaya@users.noreply.github.com>
2024-01-22 06:58:15 +0530

    Update prims.cpp

--- prims.cpp
+++ prims.cpp
@@ -4,8 +4,6 @@ using namespace std;
 int main(){
 	int N=5,m=6;
 	vector<pair<int,int> > adj[N]; 
-
-
 	adj[0].push_back({1,2});
 	adj[0].push_back({3,6});
 	adj[1].push_back({0,2});
@@ -18,37 +16,44 @@ int main(){
 	adj[3].push_back({1,8});
 	adj[4].push_back({1,5});
 	adj[4].push_back({2,7});
+
+
+
 	
-    int parent[N];
+	
+    int parent[N]; 
+      
     int key[N]; 
+      
     bool mstSet[N]; 
   
     for (int i = 0; i < N; i++) 
         key[i] = INT_MAX, mstSet[i] = false; 
+    
+    priority_queue< pair<int,int>, vector <pair<int,int>> , greater<pair<int,int>> > pq;
+
     key[0] = 0; 
     parent[0] = -1; 
-    int ansWeight = 0;
-    for (int count = 0; count < N - 1; count++)
+    pq.push({0, 0});
+
+    while(!pq.empty())
     { 
+        int u = pq.top().second; 
+        pq.pop(); 
         
-        int mini = INT_MAX, u; 
-  
-        for (int v = 0; v < N; v++) 
-{
-            if (mstSet[v] == false && key[v] < mini) 
-                mini = key[v], u = v; 
-}
-                 mstSet[u] = true; 
+        mstSet[u] = true; 
         
         for (auto it : adj[u]) {
             int v = it.first;
             int weight = it.second;
-            if (mstSet[v] == false && weight < key[v]) 
-                parent[v] = u, key[v] = weight; 
+            if (mstSet[v] == false && weight < key[v]) {
+                parent[v] = u;
+	    key[v] = weight; 
+                pq.push({key[v], v});    
+            }
         }
             
     } 
-    
     
     for (int i = 1; i < N; i++) 
         cout << parent[i] << " - " << i <<" \n"; 
